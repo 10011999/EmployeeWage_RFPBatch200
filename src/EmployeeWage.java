268991ebc7353c87ec6987a948406f5ae59ef49a
10011999 <motewaraditya77@gmail.com>
2022-09-15 16:35:55 +0530

    Added Employee Wage Working

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,17 +1,17 @@
 public class EmployeeWage {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to employeewage computation");
         final int FULL_TIME = 1;
+        final int WAGE_PER_HOUR = 20;
+        int workingHour = 0;
         int empType = (int) (Math.random() * 100) % 2;
-        if (empType == FULL_TIME)
-        {
+        if (empType == FULL_TIME) {
             System.out.println("Employee is Present");
-
-        } else
-        {
+            workingHour = 8;
+        } else {
             System.out.println("Employee is Absent");
-
         }
+        int wage = workingHour * WAGE_PER_HOUR;
+        System.out.println("Employee Daily wage is " + wage);
     }
 }
